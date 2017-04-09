cc24179d26c1e80cf2dedfb3a70db7c5af311375
adove11 <Adam@MDG-018.local>
2017-04-09 16:57:42 -0700

    add annotations and initial pins

--- index.ios.js
+++ index.ios.js
@@ -13,6 +13,7 @@ import {
   View
 } from 'react-native';
 import MapView from 'react-native-maps';
+import PlaceMap from './place_map';
 
 
 
@@ -33,11 +34,28 @@ export default class ReactNativePractice extends Component {
   }*/
 
   constructor() {
-  super();
-  this.state = {
-    selectedTab: 0
-  };
-}
+    super();
+    this.state = {
+      selectedTab: 0,
+      annotations: [
+        {
+          title: 'Smithsonian Museum',
+          latitude: 38.8980,
+          longitude: -77.0230
+        },
+        {
+          title: 'UMCP',
+          latitude: 38.9869,
+          longitude: -76.9426
+        },
+        {
+          title: 'Arlington',
+          latitude: 38.8783,
+          longitude: -77.0687
+        }
+      ]
+    };
+  }
 
 handleTabPress(tab) {
   this.setState({ selectedTab: tab })
@@ -51,9 +69,7 @@ handleTabPress(tab) {
     selected={this.state.selectedTab === 0}
     onPress={this.handleTabPress.bind(this, 0)}
   >
-    <View style={styles.view}>
-      <Text style={styles.text}>Favorite Places</Text>
-    </View>
+    <PlaceMap annotations={this.state.annotations} />
   </TabBarIOS.Item>
   <TabBarIOS.Item
     title="Place"
